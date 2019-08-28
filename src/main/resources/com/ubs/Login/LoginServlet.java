b77029dee57fe1a50248570a4a6daaa72335ae83
syk007 <deepakumarvu@gmail.com>
2019-08-28 15:22:16 +0530

    initial commit

---
+++ src/main/resources/com/ubs/Login/LoginServlet.java
@@ -0,0 +1,47 @@
+package com.ubs.Login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		String uname = request.getParameter("uname");
+		String pass = request.getParameter("pass");
+		PrintWriter printWriter=response.getWriter();
+		if(uname.equals("deepak") && pass.equals("123456")) {
+			printWriter.println("<font color=\"green\"><h1>Login Successful</h1></font>");
+		    RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");
+		    rd.forward(request,response);
+		}
+		else {
+		    printWriter.println("<font color=\"red\"><h1>Login Un-successful</h1></font>");
+		    RequestDispatcher rd=request.getRequestDispatcher("index.jsp");
+		    rd.include(request,response);
+		}
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Book Store</title>
+</head>
+<body>
+	<center>
+		<h1>Login</h1>
+		<form method="post" action="LoginServlet">
+			<br>User Name : <input type="text" name="uname" />
+			<br>
+			<br>Password : <input type="password"
+				name="pass" /> <br>
+			<br> <input type="submit">
+		</form>
+	</center>
+</body>
+</html>
